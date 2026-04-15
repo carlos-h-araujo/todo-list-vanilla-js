d9e1879940d4a3dfbc3300e1f081d2631a1e798e
Carlos Henrique Caetano de Araujo <carloshenriquecaraujo@gmail.com>
2026-04-15 01:12:12 -0300

    feat: enhance task management with input fields and status handling

--- script.js
+++ script.js
@@ -6,14 +6,43 @@ const STATUS = {
   DONE: "done",
 };
 
-const addTask = (task) => {
+const addTask = (task, status) => {
   const newTask = {
     id: Date.now(),
     text: task,
-    status: STATUS.PENDING,
+    status,
   };
 
   tasks.push(newTask);
 
   return newTask;
 };
+
+const taskInput = document.querySelectorAll(".task-input");
+
+console.log(taskInput);
+
+taskInput.forEach((input) => {
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      let actualStatus;
+      if (input.classList.contains("pending-input")) {
+        actualStatus = STATUS.PENDING;
+      } else if (input.classList.contains("doing-input")) {
+        actualStatus = STATUS.DOING;
+      } else {
+        actualStatus = STATUS.DONE;
+      }
+
+      const status = document.querySelector(`#${actualStatus}-list`);
+
+      const task = document.createElement("li");
+
+      task.textContent = input.value;
+
+      status.append(task);
+
+      addTask(input.value, actualStatus);
+    }
+  });
+});
